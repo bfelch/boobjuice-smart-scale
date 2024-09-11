d5558552ade0d3cb0ea43727fee20c33a3abbbaf
bfelch <felchbrandon@gmail.com>
2024-09-11 14:53:53 -0500

    removing displays, restoring initial installs

--- setup.sh
+++ setup.sh
@@ -2,26 +2,17 @@
 MYDIR="$(dirname $(realpath $0))"
 
 echo "beginning boobjuice setup"
-#echo "installing python3"
-#apt-get install python3
-#echo "installing pip"
-#apt-get install pip
-#echo "installing git"
-#apt-get install git
+apt-get install python3
+apt-get install pip
+apt-get install git
 
-#echo "setting up venv"
 python3 -m venv boobjuice-venv
 source boobjuice-venv/bin/activate
 
-#echo "pip installing required packages"
-#echo "${MYDIR}"
 python3 -m pip install -r "${MYDIR}/requirements.txt"
 
-#echo "installing i2c-tools"
 apt-get install i2c-tools
-#echo "cloning hx711py repo"
 git clone https://github.com/tatobari/hx711py
-#echo "cloning LCD-1602-I2C repo"
 git clone https://github.com/sterlingbeason/LCD-1602-I2C
 
 echo "setup complete!"
